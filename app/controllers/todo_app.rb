ea39839f85725e5831528c49dab6dacea75203d6
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-12 14:31:19 -0700

    Added driver code to todo_app.rb

--- app/controllers/todo_app.rb
+++ app/controllers/todo_app.rb
@@ -1,18 +1,29 @@
+require_relative "../models/tasks"
+
 class Todo
 
-  def run
+  def run(command_line_args)
+    #call something in the view
+    self.send(command_line_args[0].to_sym, command_line_args[1..-1])
   end
 
-  def add_task
+  def add()
+
+    command_line_args[1..-1]
   end
 
-  def delete_task
+  def delete
   end
 
-  def list_tasks
+  def list
   end
 
-  def complete_tasks
+  def complete
   end
-   
+
 end
+
+chore = Todo.new
+chore.run(ARGV)
+
+method(parameters)
---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+require 'faker'
+require_relative '../app/models/task'
+
+task1 = Task.new
+task1.add("something")
