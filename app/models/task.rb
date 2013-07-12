c2775103bf112a8d6e07174b556b17d8c7e26295
mjseaman <maria.pacana@gmail.com>
2013-07-12 12:19:09 -0700

    Created task.rb.shell

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,26 @@
+require_relative '../../db/config'
+
 class Task < ActiveRecord::Base
+  validates :description, presence: true
+
+  def self.list
+
+  end
+
+  def self.add(description)
+  
+  end
+
+  def self.delete(id)
+  
+  end
+
+  def delete
+
+  end
+
+  def set_complete
+    
+  end
+
 end
