b20092928fa214bab0536089767277117e538789
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-12 15:37:19 -0700

    modification to todo_app

--- app/controllers/todo_app.rb
+++ app/controllers/todo_app.rb
@@ -4,15 +4,15 @@ class Todo
 
   def run(command_line_args)
     #call something in the view
-    self.send(command_line_args[0].to_sym, command_line_args[1..-1])
+    self.send(command_line_args[0].to_sym, command_line_args)
   end
 
-  def add
+  def add(command_line_args)
     Task.add(command_line_args[1..-1].join(' '))
     # call something in the view
   end
   
-  def delete
+  def delete(command_line_args)
     Task.delete(command_line_args[1].to_i)
     # call something in the view
   end
@@ -22,7 +22,7 @@ class Todo
     # call something in the view
   end
 
-  def complete
+  def complete(command_line_args)
     Task.set_complete(command_line_args[1].to_i)
     # call something in the view
   end
